9194f0724b6f0248beb150ae9be8020d079ebd61
Olivier Maisonneuve <o.maisonneuve@lexik.fr>
2012-10-01 17:40:06 +0200

    New sample action for authentification capture.

--- Controller/SampleController.php
+++ Controller/SampleController.php
@@ -27,7 +27,37 @@ class SampleController extends Controller
             'PBX_REFUSE'       => $this->generateUrl('lexik_paybox_sample_return', array('status' => 'denied'), true),
             'PBX_ANNULE'       => $this->generateUrl('lexik_paybox_sample_return', array('status' => 'canceled'), true),
             'PBX_RUF1'         => 'POST',
-            'PBX_REPONDRE_A'   => $this->generateUrl('lexik_paybox_ipn', array(), true),
+            'PBX_REPONDRE_A'   => $this->generateUrl('lexik_paybox_ipn', array('time' => time()), true),
+        ));
+
+        return $this->render(
+            'LexikPayboxBundle:Sample:index.html.twig',
+            array(
+                'url'  => $paybox->getUrl(),
+                'form' => $paybox->getSimplePaymentForm()->createView(),
+            )
+        );
+    }
+
+    /**
+     * Sample action to call a payment.
+     */
+    public function curlAction()
+    {
+        $paybox = $this->get('lexik_paybox.request_handler');
+        $paybox->setParameters(array(
+            'PBX_CMD'          => 'AUTH'.time(),
+            'PBX_DEVISE'       => '978',
+            'PBX_PORTEUR'      => 'test@paybox.com',
+            'PBX_RETOUR'       => 'Mt:M;Ref:R;Abo:U;Auto:A;Erreur:E',
+            'PBX_TOTAL'        => '100',
+            'PBX_TYPEPAIEMENT' => 'CARTE',
+            'PBX_TYPECARTE'    => 'CB,VISA,EUROCARD_MASTERCARD',
+            'PBX_EFFECTUE'     => $this->generateUrl('lexik_paybox_sample_return', array('status' => 'success'), true),
+            'PBX_REFUSE'       => $this->generateUrl('lexik_paybox_sample_return', array('status' => 'denied'), true),
+            'PBX_ANNULE'       => $this->generateUrl('lexik_paybox_sample_return', array('status' => 'canceled'), true),
+            'PBX_RUF1'         => 'POST',
+            'PBX_REPONDRE_A'   => $this->generateUrl('lexik_paybox_ipn', array('time' => time()), true),
         ));
 
         return $this->render(
